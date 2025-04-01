b875ac342d2f632151703e53ce4663677a4b6539
Joalor64 <rhgarcon@gmail.com>
2025-04-01 12:52:58 -0400

    added navigation bar and fixed some stuff

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 // basic theme toggle
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
+const body = document.body;
 
+toggleTextChange();
+
+document.addEventListener('DOMContentLoaded', function () {
     if (localStorage.getItem('lightmode') === 'enabled') {
         body.classList.add('lightmode');
     }
-
-    document.addEventListener('keydown', (e) => {
-        if (e.key.toLowerCase() === "d") {
-            if (body.classList.contains('lightmode')) {
-                body.classList.remove('lightmode');
-                localStorage.setItem('lightmode', 'disabled');
-            } else {
-                body.classList.add('lightmode');
-                localStorage.setItem('lightmode', 'enabled');
-            }
-        }
-    });
 });
+
+function toggleTheme() {
+    if (body.classList.contains('lightmode')) {
+        body.classList.remove('lightmode');
+        localStorage.setItem('lightmode', 'disabled');
+    } else {
+        body.classList.add('lightmode');
+        localStorage.setItem('lightmode', 'enabled');
+    }
+    toggleTextChange();
+}
+
+function toggleTextChange() {
+    document.getElementById("toggle-text").innerText = (localStorage.getItem('lightmode') === 'enabled') ? "🌙" : "☀️";
+}
