abfa9f0171b3f8514fdb479cfaf6fd6d349a3693
Joalor64 <rhgarcon@gmail.com>
2025-04-01 11:21:56 -0400

    did a theme toggle thing

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// basic theme toggle
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+
+    if (localStorage.getItem('lightmode') === 'enabled') {
+        body.classList.add('lightmode');
+    }
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === " ") {
+            if (body.classList.contains('lightmode')) {
+                body.classList.remove('lightmode');
+                localStorage.setItem('lightmode', 'disabled');
+            } else {
+                body.classList.add('lightmode');
+                localStorage.setItem('lightmode', 'enabled');
+            }
+        }
+    });
+});
